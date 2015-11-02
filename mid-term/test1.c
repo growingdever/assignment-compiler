3063440945de7027ea4e0110ad255a6434244538
loki <growingdever@gmail.com>
2015-11-03 01:37:13 +0900

    multi line comment

--- mid-term/test1.c
+++ mid-term/test1.c
@@ -4,11 +4,15 @@ typedef struct s_data {
     int result;
 } data;
 
+/*
+ * pointer is useful!
+ */
 void sum(data* d) {
     d->result = d->lhs + d->rhs;
 }
 
 int main() {
+    /* user defined struct data */
     data d;
     d.lhs = 5;
     d.rhs = 10;
